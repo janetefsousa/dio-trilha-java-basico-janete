214681569da074201a26c059e66434dc2e060899
J NET <janetefsousa@gmail.com>
2023-01-08 22:54:10 -0300

    Alteração nome da classe

---
+++ tipo-variaveis/src/TiposVariaveis.java
@@ -0,0 +1,16 @@
+public class TiposVariaveis {
+    public static void main(String[] args) throws Exception {
+     //Tipos primitivos
+     //estudem a classe String que representa teste na apliacação
+     
+      String meuNome = "Janete Ferreira"
+      
+      double salarioMinimo = 2500;
+
+       short numeroCurto = 1;
+       int numeroNormal = numeroCurto;
+       short numeroCurto2 = (short) numeroNormal;
+        int numero = 5;
+
+    }
+}
