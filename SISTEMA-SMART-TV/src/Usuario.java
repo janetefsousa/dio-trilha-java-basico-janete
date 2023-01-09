784b97d31d4f402d7cee298c5c9094262deb3627
J NET <janetefsousa@gmail.com>
2023-01-09 14:41:50 -0300

    Proj-SmartTv

---
+++ SISTEMA-SMART-TV/src/Usuario.java
@@ -0,0 +1,28 @@
+public class Usuario {
+    public static void main(String[] args) throws Exception {
+       SmartTv smartTv = new SmartTv();
+
+       smartTv.diminuirVolume();
+       smartTv.diminuirVolume();
+       smartTv.diminuirVolume();
+       smartTv.aumentarVolume();
+
+       System.out.println("Canal Atual:" +smartTv.canal);
+       smartTv.mudarCanal(13);
+       System.out.println("Novo Canal:" +smartTv.canal);
+
+
+      System.out.println("Tv Ligada ?" + smartTv.ligada);
+      System.out.println("Qual canal ?" + smartTv.canal);
+      System.out.println("Qual volume ?" +smartTv.volume);
+
+       smartTv.ligar();
+       System.out.println("Novo Status -> Tv Ligada ?" + smartTv.ligada);
+       
+       smartTv.desligar();
+       System.out.println("Novo Stuatus -> Tv Ligada ? " + smartTv.ligada);
+
+
+
+    }
+}
